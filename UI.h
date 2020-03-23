8056d4a25871f23fb6fe3bd75729eb065f408f4d
stoiagianina <62550818+stoiagianina@users.noreply.github.com>
2020-03-23 19:02:59 +0200

    Create UI.h

---
+++ UI.h
@@ -0,0 +1,17 @@
+#include "C:\\Users\user\source\repos\lab5\lab5\Header.h"
+class UI
+{
+private:
+	Controller *ctrl;
+public:
+	UI();
+	~UI();
+
+	void citire();
+
+	void execute();
+	void print_menu();
+	void menu();
+
+
+};
